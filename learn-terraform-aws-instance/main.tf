e0b140ec8eeec96f89cbdebaa4708b685124370a
yasuaki640 <yasuaki640@gmail.com>
2023-09-17 18:21:26 +0900

    ああああああああああああああああああ

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+data "aws_ami" "amzlinux2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.amzlinux2.id
+  instance_type = "t2.medium"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
