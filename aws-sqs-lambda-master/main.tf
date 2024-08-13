904cf4258a03594b1773dc93563ae75a953149ec
yasuaki640 <yasuaki640@gmail.com>
2024-08-13 22:17:48 +0900

    Remove commented-out resources and streamline IAM role description.

--- aws-sqs-lambda-master/main.tf
+++ aws-sqs-lambda-master/main.tf
@@ -1,4 +1,3 @@
-// IAM Role for Lambda Function
 resource "aws_iam_role" "default" {
   name               = var.service_name
   description        = "IAM Role for ${var.service_name}"
@@ -16,7 +15,6 @@ resource "aws_iam_role_policy_attachment" "default" {
   policy_arn = aws_iam_policy.default.arn
 }
 
-// Lambda Function Resources
 resource "aws_cloudwatch_log_group" "default" {
   name              = "/aws/lambda/${var.service_name}"
   retention_in_days = 7
@@ -38,21 +36,6 @@ resource "aws_lambda_function" "default" {
   timeout = 120
 }
 
-// TODO SNS Resources
-/*
-resource "aws_sns_topic" "default" {
-  name            = var.service_name
-  delivery_policy = file("sns_delivery_policy.json")
-}
-
-resource "aws_sns_topic_subscription" "default" {
-  topic_arn = aws_sns_topic.default.arn
-  protocol  = "lambda"
-  endpoint  = aws_lambda_function.default.arn
-}
-*/
-
-// SQS
 resource "aws_sqs_queue" "default" {
   name                       = "${var.service_name}_sqs"
   visibility_timeout_seconds = 12
@@ -63,17 +46,16 @@ resource "aws_sqs_queue" "default" {
   })
 }
 
-// Lambda SQS mapping
 resource "aws_lambda_event_source_mapping" "default" {
   event_source_arn = aws_sqs_queue.default.arn
   function_name    = aws_lambda_function.default.arn
 }
 
-// Dead Letter Queue
 resource "aws_sqs_queue" "default_deadletter" {
   name = "${var.service_name}_sqs_dlq"
 }
 
+// dead letter queue
 resource "aws_sqs_queue_redrive_allow_policy" "terraform_queue_redrive_allow_policy" {
   queue_url = aws_sqs_queue.default_deadletter.id
 
