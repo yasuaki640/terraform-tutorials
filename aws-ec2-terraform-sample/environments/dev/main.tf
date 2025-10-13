a4d6b38453716ce1709ad09e11e62450df32e992
yasuaki640 <yasuaki640@gmail.com>
2025-10-13 16:37:56 +0900

    Refactor VPC CIDR to use variable
    - Extract hardcoded VPC CIDR block to variable
    - Define vpc_cidr variable in variables.tf with default value
    - Reference variable in main.tf module call

--- aws-ec2-terraform-sample/environments/dev/main.tf
+++ aws-ec2-terraform-sample/environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
   source = "../../modules/vpc"
 
-  vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = var.vpc_cidr
 }
--- aws-ec2-terraform-sample/environments/dev/variables.tf
+++ aws-ec2-terraform-sample/environments/dev/variables.tf
@@ -10,11 +10,11 @@
 #   default     = "dev"
 # }
 #
-# variable "vpc_cidr" {
-#   description = "CIDR block for VPC"
-#   type        = string
-#   default     = "10.0.0.0/16"
-# }
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 #
 # variable "public_subnet_cidr" {
 #   description = "CIDR block for public subnet"
